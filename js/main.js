da9a89d49ba8b3db3e600aa49c3bd9b0f1138ae5
mateusz socko <dataochi@gmail.com>
2021-04-18 19:01:40 +0200

    zadanie29

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let firstNumber = 20;
+let secondNumber = 40;
+function sum(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber ;
+    return result ;
+}
+console.log(sum(firstNumber, secondNumber)) ;
