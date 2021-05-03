793ea6b9d482be1fea2e5b13b93fc52eacb7f93c
Mateusz <dataochi@gmail.com>
2021-05-03 20:29:45 +0200

    poprawka

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let firstNumber = 20;
 let secondNumber = 40;
 function sum(number1, number2){
-    let result = number + number ;
+    let result = number1 + number2 ;
     return result ;
 }
 console.log( sum(firstNumber, secondNumber) ) ;
